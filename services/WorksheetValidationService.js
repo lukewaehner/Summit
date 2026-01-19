af1e37d274f83a51233879c21d07c60cbb0fb559
Luke Waehner <37313537+lukewaehner@users.noreply.github.com>
2026-01-18 20:46:08 -0500

    Update WorksheetValidationService.js

--- services/WorksheetValidationService.js
+++ services/WorksheetValidationService.js
@@ -433,6 +433,7 @@ const WorksheetValidationService = {
       // NOTE: Does this need actualRow removed? If yes, will this propgate correctly.
       // But, after one of the duplicates is processed the next will be done on the next passthrough
       // Logically, clear duolicates further down the sheet?
+      // Store an array of previosuly seen file keys, check against that and clear after
       const uniqueKey = `${student.name}|${fileId}|${actualRow}`;
       if (existingItems.has(uniqueKey)) {
         filesSkipped++;
